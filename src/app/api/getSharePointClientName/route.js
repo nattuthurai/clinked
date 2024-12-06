6240df92a943040e780a625cfed76c73d3e52478
nattuthurai <nattuthurai.k@live.com>
2024-12-06 11:38:25 +0530

    upload

--- src/app/api/getSharePointClientName/route.js
+++ src/app/api/getSharePointClientName/route.js
@@ -1,18 +1,11 @@
 import axios from "axios";
 import { NextResponse } from 'next/server';
 
-// const tenantId = "1d4a7043-d2a8-4578-8026-5d2221fbf8ec";
-// const clientId = "f0a024fb-c3c8-43c5-b41a-bf5d5c6690cf";
-// const clientSecret = "9aK8Q~ZW6i_QIZn6HfhUAGTTlQ0Nw6G1L89sndyc";
-// const siteId = "d726e000-2148-4faf-b270-e617010a782c";
-// const listId = "13f5df06-1ffa-4c1b-821c-7c41e621f404";
-
 const tenantId = process.env.TENANT_ID;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const siteId = process.env.SITE_ID;
 const listId = process.env.LIST_CLIENTMAPPING_ID;
-
 
 async function getAccessToken() {
   const url = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`;
