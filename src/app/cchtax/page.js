39fa0e63c91b03dd4e21045f31d6c915a1950550
nattuthurai <nattuthurai.k@live.com>
2024-12-22 19:17:03 +0530

    fixed design and year issue

--- src/app/cchtax/page.js
+++ src/app/cchtax/page.js
@@ -135,28 +135,41 @@ export default function HomePage() {
 
         setDataDropdown(dropdownData);
 
-        dropdownYearData.push(
-          {
-            value: "2020",
-            label: "2020",
-          },
-          {
-            value: "2021",
-            label: "2021",
-          },
-          {
-            value: "2022",
-            label: "2022",
-          },
-          {
-            value: "2023",
-            label: "2023",
-          },
-          {
-            value: "2024",
-            label: "2024",
-          }
-        );
+        const currentYear = new Date().getFullYear();
+
+        for (let year = currentYear; year >= currentYear - 10; year--) {
+          dropdownYearData.push({
+            value: year.toString(),
+            label: year.toString(),
+          });
+        }
+
+        // dropdownYearData.push(
+        //   {
+        //     value: "2019",
+        //     label: "2019",
+        //   },
+        //   {
+        //     value: "2020",
+        //     label: "2020",
+        //   },
+        //   {
+        //     value: "2021",
+        //     label: "2021",
+        //   },
+        //   {
+        //     value: "2022",
+        //     label: "2022",
+        //   },
+        //   {
+        //     value: "2023",
+        //     label: "2023",
+        //   },
+        //   {
+        //     value: "2024",
+        //     label: "2024",
+        //   }
+        // );
 
         setDataYearDropdown(dropdownYearData);
       } catch (error) {
